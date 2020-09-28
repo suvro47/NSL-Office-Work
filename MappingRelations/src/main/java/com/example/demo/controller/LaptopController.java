ffe0ab23b4eb6b4d4506ccb3fedd363213cc2460
suvro47 <suvro047@gmail.com>
2020-09-28 17:09:19 +0600

    mapping_relations

---
+++ MappingRelations/src/main/java/com/example/demo/controller/LaptopController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Laptop;
+import com.example.demo.repository.LaptopRepo;
+
+
+
+@RestController
+public class LaptopController {
+	
+	@Autowired
+	LaptopRepo lrepo;
+	
+	@GetMapping("/laptops")
+	public List<Laptop> getLaptops(){
+		return lrepo.findAll();
+	}
+	
+	
+	
+
+}
