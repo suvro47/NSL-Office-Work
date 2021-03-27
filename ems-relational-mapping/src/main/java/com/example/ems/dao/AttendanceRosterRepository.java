1b9d1eee9a385590d9adde9c60364a44c9952e57
suvro47 <suvro047@gmail.com>
2021-03-27 21:13:42 +0600

    sample project removed from master

--- ems-relational-mapping/src/main/java/com/example/ems/dao/AttendanceRosterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ems.models.AttendanceRoster;
-
-public interface AttendanceRosterRepository extends JpaRepository< AttendanceRoster, Long >{
-
-}
--- ems-relational-mapping/src/main/java/com/example/ems/dao/BankDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ems.models.BankDetails;
-
-public interface BankDetailsRepository extends JpaRepository<BankDetails, Long>{
-
-}
