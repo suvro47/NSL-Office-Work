65c36674d3d3c040544b3cd5b52d004a4fe8ee6f
suvro47 <suvro047@gmail.com>
2021-03-27 21:11:56 +0600

    mvc project removed from master

--- spring-security-II/src/main/java/com/example/demo/UzerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UzerRepository extends JpaRepository<Uzer, Long>{
-	
-	Uzer findByUsername( String username);
-
-}
