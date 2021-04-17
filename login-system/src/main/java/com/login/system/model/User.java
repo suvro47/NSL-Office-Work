18ffa529377ebd82fcd8cbb53cc1bd9ef6d56d33
suvro47 <suvro047@gmail.com>
2021-04-17 17:21:38 +0600

    login-logout mvc

---
+++ login-system/src/main/java/com/login/system/model/User.java
@@ -0,0 +1,48 @@
+package com.login.system.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
